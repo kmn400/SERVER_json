02fdd66933da22f02ba23a83fc6e70e1d4d54d57
minho <kmn400@nate.com>
2021-12-20 11:09:01 +0900

    211220_서버와 소통하는 방법 (FUTURE)

--- lib/UI/TEST/test_screen.dart
+++ lib/UI/TEST/test_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -16,7 +15,24 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   Album? _album;
 
+  //flutter 문서에서 cookbook에서 network에서 http패키지 넣고,
+  // 네트워크 요청하는 리퀘스트 권한
+  // 받아온 데이터를 http.리스폰스 객체 안에, 다트에서 사용하고자 할 때,
+  // 코드를 변환해야 하는데, fromjson 생성자를 만들어서 암묵적으로
+  //이렇게 하자고 정리가 됨.
+  // json가지고 객체를 만들 때, factory 쓰자고 암묵적인 약속을 정함.
+
+  Future<void> init() async {
+    //async에서는 await를 써저야 함.
+    Album album =
+        await fetchAlbum(); //미래에 답이 와야 되기 때문에 'await'를 써줘야 함. 아래 then 코드와 동일
+    setState(() {
+      _album = album;
+    });
+  }
+
   //async 와 await 는 세트임.
+  //uri에서 정보를 받아와야 하기 떄문에, 기다림이 필요.
   Future<Album> fetchAlbum() async {
     //await [future가 리턴되는 코드]
     final response = await http
@@ -36,22 +52,79 @@ class _TestScreenState extends State<TestScreen> {
   @override
   void initState() {
     super.initState();
-    fetchAlbum().then((album) {
-      setState(() {
-        _album = album;
-      });
-    });
+
+    init();
+
+    // fetchAlbum().then((album) {
+    //   setState(() {
+    //     _album = album;
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Network Sample'),
+        title: const Text('Network Sample'),
       ),
-      body: Container(
-        child: Text(_album.toString()),
+
+      //예를 들어, 날씨 바꾸거나 실시간 정보들을 불러일으킬 때, futurer builder를 사용
+      //setstate를 사용하지 않음.
+      body: FutureBuilder<Album>(
+        future: fetchAlbum(),
+        builder: (context, snapshot) {
+
+          if(snapshot.hasError) {
+            return const Center(child: Text('네트워크 에러!!!'));
+          }
+          // 연결상태를 보는 것.
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          //데이터가 없다면
+          if(!snapshot.hasData) {
+            return const Center(child:  Text('데이터가 없습니다'));
+
+          }
+          //데이터가 여기에서는 무조건 있는 상황
+          final Album album = snapshot.data!;
+
+
+          //여기가 데이터를 ui로 구현하는 메서드
+          return Text(
+            '${album.id} : ${album.toString()}',
+            style: const TextStyle(fontSize: 30),
+          );
+        },
       ),
+
+
+
+     /*
+     _album == null
+
+          // 앨범이 서버에 요청하고 요구 받는 동안에는
+          // 앨범 값이 'null'임. 그 간극을 '로딩'으로 표시해야 함
+
+          ? const Center(child: const CircularProgressIndicator())
+          : Text(
+              '${_album!.id} : ${_album.toString()}',
+              style: const TextStyle(fontSize: 30),
+            ), */
     );
+  }
+
+
+  //***
+  // widget _buildBody(Album album){
+  //   return Text(
+  //     '${album.id} : ${album.toString()}',
+  //     style: const TextStyle(fontSize: 30),
+  //   );
+  // },
+
+    )
   }
 }
